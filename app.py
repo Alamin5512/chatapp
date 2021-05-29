41a84d31aca137468fc0d3aaadfcb1ac7e920ffa
Alamin5512 <56962724+Alamin5512@users.noreply.github.com>
2021-05-29 07:28:46 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,39 +1,62 @@
-from flask import *
-from sklearn.externals import joblib
-import json
-import os
-import pandas as pd
-from pandas.io.json import json_normalize
-import numpy as np
-
+from flask import Flask, request, Response
+import requests, json, random, os
 app = Flask(__name__)
 
+# env_variables
+# token to verify that this bot is legit
+verify_token = os.getenv('ALAMIN', None)
+# token to send messages through facebook messenger
+access_token = os.getenv('EAAFpqWNlVmYBANhpnolxaCFxGgH2O9ySI2aCd3dXE9hwSJ8T7EUZCImxXWJrqGxrrDpqqkgiR2RXEVvOrAUg9hmPToAyPzl6a9FZAqYicmBTRFmV9uVcFtCOnHeXUwb4CeNLQdLXvzaS5e60ObtmV861lm5N36qycSC2xz36HHvXSIRqPG', None)
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
+@app.route('/webhook', methods=['GET'])
+def webhook_verify():
+    if request.args.get('hub.verify_token') == verify_token:
+        return request.args.get('hub.challenge')
+    return "Wrong verify token"
 
+@app.route('/webhook', methods=['POST'])
+def webhook_action():
+    data = json.loads(request.data.decode('utf-8'))
+    for entry in data['entry']:
+        user_message = entry['messaging'][0]['message']['text']
+        user_id = entry['messaging'][0]['sender']['id']
+        response = {
+            'recipient': {'id': user_id},
+            'message': {}
+        }
+        response['message']['text'] = handle_message(user_id, user_message)
+        r = requests.post(
+            'https://graph.facebook.com/v2.6/me/messages/?access_token=' + access_token, json=response)
+    return Response(response="EVENT RECEIVED",status=200)
 
-@app.route('/predict', methods=["post"])
-def predict():
-    formals = request.form
-    path1 = "/static/json/"
-    with open(os.path.join(os.getcwd() + "/" + path1, 'file.json'), 'w') as f:
-        json.dump(formals, f)
-    with open(os.path.join(os.getcwd() + "/" + path1, 'file.json'), 'r') as f:
-        values = json.load(f)
-    df = pd.DataFrame(json_normalize(values))
-    model_path = os.getcwd() + "/static/model/diabetes.pkl"
-    model = joblib.load(model_path)
-    result = model.predict(df)
-    a = np.array(1)
-    if result.astype('int') == a.astype('int'):
-        msg = "Success"
-    else:
-        msg = "Unsuccess"
-    positive_percent = model.predict_proba(df)[0][1] * 100
-    return render_template("index.html", msg=msg, prob=positive_percent, **request.args)
+@app.route('/webhook_dev', methods=['POST'])
+def webhook_dev():
+    # custom route for local development
+    data = json.loads(request.data.decode('utf-8'))
+    user_message = data['entry'][0]['messaging'][0]['message']['text']
+    user_id = data['entry'][0]['messaging'][0]['sender']['id']
+    response = {
+        'recipient': {'id': user_id},
+        'message': {'text': handle_message(user_id, user_message)}
+    }
+    return Response(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
 
+def handle_message(user_id, user_message):
+    # DO SOMETHING with the user_message ... ¯\_(ツ)_/¯
+    return "Hello "+user_id+" ! You just sent me : " + user_message
+
+@app.route('/privacy', methods=['GET'])
+def privacy():
+    # needed route if you need to make your bot public
+    return "This facebook messenger bot's only purpose is to [...]. That's all. We don't use it in any other way."
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello there, I'm a facebook messenger bot."
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
